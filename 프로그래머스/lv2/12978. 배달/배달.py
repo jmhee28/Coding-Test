34e945b4b1279066187049f45e890eb164867b2a
Joung MyeongHee <92200502+jmhee28@users.noreply.github.com>
2023-02-08 17:46:53 +0900

    [level 2] Title: 배달, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -0,0 +1,34 @@
+import heapq
+INF = int(1e9)
+
+def solution(N, road, K):
+    answer = 0
+    distance = [INF] * (N+1)
+    
+    graph = [[] for i in range(N+1)]
+    for r in road:
+        graph[r[0]].append((r[1], r[2]))
+        graph[r[1]].append((r[0], r[2]))
+        
+    q = []
+    heapq.heappush(q, (0, 1))
+    distance[1] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+    # print(distance)
+    for i in range(1, N+1):
+        if distance[i] <= K:
+            answer += 1
+        
+        
+        
+
+    return answer
