f0c378da986cf53763eefb30d160e31184fe6fd2
Joung MyeongHee <92200502+jmhee28@users.noreply.github.com>
2023-02-28 17:36:21 +0900

    [Silver II] Title: 민겸 수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.py
+++ 백준/Silver/21314. 민겸 수/민겸 수.py
@@ -1,42 +1,26 @@
+import sys
+input = sys.stdin.readline
 s = input()
-s = list(s)
-n = len(s)
 
-def getMax(s, n):
-    ans =''
-    idx = 0
-    while idx < n :
-        mcnt = 0
-        if s[idx] == 'M':
-            while idx < n and s[idx] == 'M':
-                idx += 1
-                mcnt += 1
-            if idx < n and s[idx] == 'K':
-                idx += 1
-                x = 5 * (10 ** (mcnt))
-                ans += str(x)
-            elif idx == n and s[idx-1] == 'M':
-                for i in range(mcnt):
-                    ans += '1'
-        else:
-            ans += '5'
-            idx += 1
-    return ans
-def getMin(s, n):
-    ans = ''
-    idx = 0
-    
-    while idx < n:
-        mcnt = 0
-        if s[idx] == 'M':
-            while idx < n and s[idx] == 'M':
-                idx += 1
-                mcnt += 1
-            ans += str(10 ** (mcnt-1))
-        else:
-            idx+=1
-            ans += '5'
-    return ans
-print(getMax(s, n))
-print(getMin(s, n))
+s_max = ''
+s_min = ''
+m = 0
 
+for i in range(len(s)):
+    if s[i] == 'M':
+        m += 1
+    elif s[i] == 'K':
+        if m:
+            s_max += str(5 * (10 ** m))
+            s_min += str(10 ** m + 5)
+        else:
+            s_max += '5'
+            s_min += '5'
+        m = 0
+if m:
+    s_min += str(10 ** (m-1))
+    while m:
+        s_max += '1'
+        m -= 1
+print(s_max)
+print(s_min)
