e57d6ac5dfe20fc8633a98c64ab26703e1303a93
Joung MyeongHee <92200502+jmhee28@users.noreply.github.com>
2023-03-01 18:48:42 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1504 ms, Memory: 161652 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,40 +1,28 @@
-import sys
-import heapq
+from collections import deque
 
-input = sys.stdin.readline
-INF = int(1e9)
+n,m,k,x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
 
-n, m, k, x = map(int, input().split())
-distance = [INF] * (n+1)
-graph = [[] for i in range(n+1)]
-
-for i in range(m):
+for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append((b, 1))
+    graph[a].append(b)
 
-def dijk(start):
-    q =[]
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
+distance = [-1] * ( n + 1 )
 
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+q = deque()
+distance[x] = 0
+q.append(x)
 
-dijk(x)
-answer = []
+while q:
+    a = q.popleft()
+    for i in graph[a]:
+        if distance[i] == -1:
+            distance[i] = distance[a] + 1
+            q.append(i)
+flag = False
 for i in range(1, n+1):
     if distance[i] == k:
-        answer.append(i)
-if len(answer) == 0:
-    print(-1)
-else:
-    answer.sort()
-    for i in answer:
         print(i)
+        flag = True
+if flag == False:
+    print(-1)
