846531f0682d73ed8a073305b769db7fbfb3ffd2
Joung MyeongHee <92200502+jmhee28@users.noreply.github.com>
2023-06-09 00:45:41 +0900

    [unrated] Title: 미로 탈출, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,59 @@
+from collections import deque
+
+board = []
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+sx, sy, ex, ey, lx, ly = 0, 0, 0, 0, 0, 0
+
+
+def bfs(x, y, n, m, target):
+    q = deque()
+    visited = [[-1 for _ in range(m)] for _ in range(n)]
+    visited[x][y] = 0
+    q.append((x, y))
+    while q:
+        x, y = q.popleft()
+        if board[x][y] == target:
+            break
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (
+                0 <= nx < n
+                and 0 <= ny < m
+                and board[nx][ny] != "X"
+                and visited[nx][ny] == -1
+            ):
+                visited[nx][ny] = visited[x][y] + 1
+                q.append((nx, ny))
+    if target == "L":
+        return visited[lx][ly]
+    return visited[ex][ey]
+
+
+def solution(maps):
+    global sx, sy, ex, ey, lx, ly
+    n = len(maps)
+    m = len(maps[0])
+    for i in range(n):
+        board.append(list(maps[i]))
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == "S":
+                sx, sy = i, j
+            if board[i][j] == "E":
+                ex, ey = i, j
+            if board[i][j] == "L":
+                lx, ly = i, j
+    startToExit = bfs(sx, sy, n, m, "E")
+    toLever = bfs(sx, sy, n, m, "L")
+    leverToExit = bfs(lx, ly,n, m, "E")
+    if toLever == -1 or leverToExit == -1 :
+        return -1
+    else:
+        return toLever+leverToExit
+        # return min(startToExit, toLever+leverToExit)
+    
+
+
+# solution(["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"])
