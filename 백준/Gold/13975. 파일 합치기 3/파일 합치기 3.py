a499decf9595417d81c143046664ef5909bcc611
Joung MyeongHee <jmhee3411@gmail.com>
2023-10-22 17:21:40 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 5080 ms, Memory: 152724 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -0,0 +1,30 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+T = int(input())
+def getAns(heap):
+    result = 0
+    while 1:
+        if len(heap) == 1:
+            # result += heapq.heappop(heap)
+            return result
+        elif len(heap) == 0:
+            return result
+        a = heapq.heappop(heap)
+        b = heapq.heappop(heap)
+        result += (a+b)
+        heapq.heappush(heap,(a+b))
+        
+for t in range(T):
+    K = int(input())
+    datas = list(map(int, input().split()))
+    heapq.heapify(datas) 
+    answer = getAns(datas)   
+    # print()
+    print(answer)
+    
+
+
+
+    
