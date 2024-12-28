9979d6aeb8ca19f61bd68ea9fcbae75559e8319d
Joung MyeongHee <jmhee3411@gmail.com>
2024-12-28 23:34:22 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+for t in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())
+    
+    dp = [0] * (M + 1)
+    
+    dp[0] = 1
+    
+    for i in range(N):
+        for j in range(coins[i], M + 1):
+            dp[j] += dp[j - coins[i]]
+    print(dp[M])
