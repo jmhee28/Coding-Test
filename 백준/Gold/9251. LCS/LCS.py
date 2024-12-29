44827f496ba970e5fcea97de041fcf34f91c10a5
Joung MyeongHee <jmhee3411@gmail.com>
2024-12-29 18:59:19 +0900

    [Gold V] Title: LCS, Time: 476 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,36 +1,18 @@
-astr = input()
-bstr = input()
-n = len(astr)
-m = len(bstr)
+s1 = input()
+s2 = input()
 
-LCS = [[0 for _ in range(m+1)] for _ in range(n+1)]
-result = []
+m = len(s1)
+n = len(s2)
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if astr[i-1] == bstr[j-1]:
-            LCS[i][j] = LCS[i-1][j-1] + 1
+lcs = [[0] * (n + 1) for _ in range(m + 1)]
+
+for i in range(1, m + 1):
+    for j in range(1, n + 1):
+        if s1[i-1] == s2[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
         else:
-            LCS[i][j] = max(LCS[i][j-1], LCS[i-1][j])
+            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
             
-cur  = LCS[n][m]     
-r, c = n, m  
-while 1:
-    if cur == 0:
-        break
-    if cur == LCS[n-1][m]:
-        cur = LCS[n-1][m]
-        n -= 1
-    elif cur == LCS[n][m-1]:
-        cur = LCS[n][m-1]
-        m -= 1
-    else:
-        result.append(bstr[m-1])
-        cur = LCS[n-1][m-1]
-        n-=1
-        m-=1
-        
-print(len(result))    
-             
-# for i in range(len(result)-1, -1 , -1):
-#     print(result[i], end='')
+print(lcs[m][n])
+            
+    
