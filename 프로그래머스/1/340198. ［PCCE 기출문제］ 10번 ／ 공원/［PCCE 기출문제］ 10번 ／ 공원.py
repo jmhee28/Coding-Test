b5142d744eb402a5c854745d5d16714bbe9427b7
Joung MyeongHee <jmhee3411@gmail.com>
2025-01-12 02:21:50 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 0.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,34 +1,24 @@
-n = 0
-m = 0
-gpark = []
-def search(x, y):
-    length = 1
-    while True:
-        for i in range(x, x + length):
-            for j in range(y, y + length):
-                if not (0 <= i < n and 0 <= j < m and gpark[i][j] == "-1"):
-                    return length
-        length += 1
-    return length
-
 def solution(mats, park):
-    global n, m, gpark
-    gpark = park
-    answer = -1
-    max_len = -1
     n = len(park)
     m = len(park[0])
+    dp = [[0] * m for _ in range(n)]
     
+    max_len = 0  # 최대 정사각형 변의 길이
+
+    # DP 테이블 채우기
     for i in range(n):
         for j in range(m):
             if park[i][j] == "-1":
-                cur_len = search(i, j) - 1
-                max_len = max(max_len, cur_len)
+                if i == 0 or j == 0:  # 테두리 처리
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                max_len = max(max_len, dp[i][j])
     
+    # 정렬된 매트 크기에서 최대 정사각형 길이에 맞는 값 찾기
     mats.sort(reverse=True)
-    
     for mat in mats:
         if mat <= max_len:
             return mat
     
-    return answer
+    return -1
