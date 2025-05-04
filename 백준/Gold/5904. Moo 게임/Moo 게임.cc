357c92654801fc7586c04bb916ec0ac574dd7845
Joung MyeongHee <jmhee3411@gmail.com>
2025-05-04 23:18:41 +0900

    [Gold V] Title: Moo 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/5904. Moo 게임/Moo 게임.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+int K;
+int N;
+vector<long long> mooSizes(50, 0);
+long long mooSize = 0;
+string fmoo = "moo";
+long long getMooSize(int k) { 
+    if(mooSizes[k] != 0) {
+        return mooSizes[k];
+    }
+    int prev = (getMooSize(k - 1) * 2);
+    mooSizes[k] =  prev + (k + 2) + 1;
+    return mooSizes[k];
+}
+
+
+char getAnswer(int k) {
+    if(k == 0){
+        return fmoo[N-1];
+    }
+    if(N > mooSizes[k-1] && N < mooSizes[k-1] + (k + 2 + 1) + 1){
+       if(N == mooSizes[k-1] + 1){
+        return 'm';
+       }else{
+        return 'o';
+       }
+    }
+    else if(N <= mooSizes[k-1]){
+        return getAnswer(k-1);
+    } else if(N >= mooSizes[k-1] + (k + 2 + 1) + 1){
+        N -= mooSizes[k-1] + (k + 2 + 1);
+        return getAnswer(k-1);
+    }
+    return '\0';
+}
+
+int main() {
+    mooSizes[0] = 3;
+    K = 0;
+    cin >> N;
+    // N = 23;
+    
+
+    while(true) {
+        long long mooSize = getMooSize(K);
+        if (mooSize >= N) {
+            break;
+        }
+        K++;
+    }
+    char answer = getAnswer(K);
+    cout << answer << '\n';
+}
